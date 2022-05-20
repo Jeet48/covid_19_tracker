d43383241d93b5edc4cd4f770ab5de97bef72c7b
Jeet Mondal <97789227+SaltyLasagna@users.noreply.github.com>
2022-05-21 02:32:24 +0530

    Implemented different sections of the covid app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, {useState} from "react";
 
 import {useEffect} from "react";
 
-import { MenuItem,  Select ,FormControl } from '@mui/material';
+import { MenuItem,  Select ,FormControl, Card } from '@mui/material';
 
 import './App.css';
+
+import InfoBox from './InfoBox';
+
+import Map from './Map';
+import { CardContent } from "@material-ui/core";
 
 function App() {
 
   const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
-  
+  const [country, setCountry] = useState('worldwide');
 
 
   useEffect(() => {
@@ -33,15 +38,20 @@ function App() {
   },[]); 
 
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  }
+
   return (
   <div className="app">
-    <div className="app__header">
+   <div className="app__left">
+   <div className="app__header">
     <h1>COVID-19 TRACKER</h1>
     <FormControl className="app__dropdown">
-      <Select
-        variant="outlined"
-        value="abc"
-      >
+      <Select variant="outlined" onChange = {onCountryChange} value={country}>
+       <MenuItem value="worldwide">Worldwide</MenuItem>
       {/* Loop through all the countries and show a drop down list of option */}
           
           {
@@ -57,19 +67,30 @@ function App() {
     </Select>
     </FormControl>
     </div>
-   
+  
 
-    {/*Header */}
-    {/*Title + select input dropdown field*/}
+    <div className="app__stats">
+      <InfoBox title="Coronavirus Cases" cases={123} total={2000}/>
 
+      <InfoBox title="Recovered" cases={1243} total={3000}/>
+
+      <InfoBox title="Deaths" cases={12378} total={4000}/>
     {/* InfoBoxs */}
     {/* InfoBoxs */}
     {/* InfoBoxs */}
-
+    </div>
     {/*Table */}
     {/*Graph */}
 
     {/*Map */}
+   </div>
+   <Card className="app_right">
+      <CardContent>
+        <h3>Live Cases by Country</h3>
+
+        <h3>World wide cases</h3>
+      </CardContent>
+   </Card>
     </div>
   );
 }
